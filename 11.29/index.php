75eae4aa01f50dd9ebb687173a766fc6a2f59545
piaodong <2470493750@qq.com>
2018-11-30 10:07:21 +0800

    日考

---
+++ 11.29/index.php
@@ -0,0 +1,48 @@
+<?php 
+
+echo "数组";	
+
+// array_count_values // 统计数组中所有的值出现的次数
+
+
+// $A = array(1, "hello", 1, "world", "hello");
+// print_r(array_count_values ($A));
+
+// array_pop — 将数组最后一个单元弹出
+
+// $stack = array("orange", "banana", "apple", "raspberry");
+// $fruit = array_pop($stack);
+// print_r($stack);
+
+
+
+// array_push — 将一个或多个单元压入数组的末尾
+
+// $stack = array("orange", "banana");
+// array_push($stack, "apple", "raspberry");
+// print_r($stack);
+
+
+// array_sum — 计算数组中所有值的和 
+
+// $a = array(2, 4, 6, 8);
+// echo "sum(a) = " . array_sum($a) . "\n";
+
+// $b = array("a" => 1.2, "b" => 2.3, "c" => 3.4);
+// echo "sum(b) = " . array_sum($b) . "\n";
+
+
+// array_unique — 移除数组中重复的值
+
+// $input = array("a" => "green", "red", "b" => "green", "blue", "red");
+// $result = array_unique($input);
+// print_r($result);
+
+
+echo '字符串';
+
+// str_repeat — 重复一个字符串
+
+echo str_repeat("-=", 10);
+
+?>
